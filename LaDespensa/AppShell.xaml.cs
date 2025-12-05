071ecad1793a8f0e5c619cda91d0ab78e2cfd4c4
Cristina-Ch-Dr <cristinachvartatska@gmail.com>
2025-12-05 14:46:31 +0100

    Agregar archivos de proyecto.

---
+++ LaDespensa/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LaDespensa
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LaDespensa/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "LaDespensa")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "LaDespensa.Pages")]
